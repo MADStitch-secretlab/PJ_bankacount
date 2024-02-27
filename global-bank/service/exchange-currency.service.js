e12f05eae20ad03ee2f6c55baf71577437892ee8
MADStitch-secretlab <limdonghyuk12@gmail.com>
2024-02-27 14:02:37 +0900

    refactor : 환전, 환율 조회 서비스 분리

---
+++ global-bank/service/exchange-currency.service.js
@@ -0,0 +1,51 @@
+import {promiseReadLine, promiseReadLineWithValidationFns} from "../../common/utils/promise-readline.utils.js";
+import {MESSAGE} from "../const/message.const.js";
+import {isNumber} from "../helper/check-is-it-num.helper.js";
+import {checkIsItCurrency} from "../utils/check-isit-currency.utils.js";
+import {exchangeComparing} from "../helper/exchage-comparing.helper.js";
+import {compareExchangeRate} from "../helper/compare-exchange-rate.helper.js";
+import {findCurrencyAmount} from "../repository/find-money-amount.repository.js";
+import {updateAccount} from "../repository/update-account.repository.js";
+import {showAccount} from "./inquiry.service.js";
+
+const exchangeCurrency = async (string) => {
+  try {
+    const from = await promiseReadLineWithValidationFns({
+      question: MESSAGE.EXCHANGE.FROM,
+      validationFnList: [checkIsItCurrency],
+      transformFn: (currency) => currency.toUpperCase(),
+    });
+    const to = await promiseReadLineWithValidationFns({
+      question: MESSAGE.EXCHANGE.TO,
+      validationFnList: [checkIsItCurrency],
+      transformFn: (currency) => currency.toUpperCase(),
+    });
+
+    if (from === to) throw new Error(MESSAGE.EXCHANGE.SAME_CURRENCY);
+    const amount = await promiseReadLineWithValidationFns({
+      question: MESSAGE.EXCHANGE.AMOUNT,
+      validationFnList: [isNumber],
+      transformFn: (amount) => parseFloat(amount, 1),
+    });
+    const fromMoney = await findCurrencyAmount(from);
+    const toMoney = await findCurrencyAmount(to);
+    // 환전할 금액(10% 부과한 금액)이 충분한지 확인
+    const fromAmount = await exchangeComparing(fromMoney, amount);
+    // 환전 후 금액
+
+    const exchangeRate = await compareExchangeRate(to, from);
+    const result = exchangeRate * amount + toMoney;
+    // 환전 정보 sql에 저장
+    await updateAccount(from, fromAmount);
+    await updateAccount(to, result);
+    /*
+     * await updateRepository({ KRW : 30000, USD : 100 })
+     * */
+    await console.log(MESSAGE.EXCHANGE.SUCCESS);
+    await showAccount();
+  } catch (e) {
+    console.log(e.message);
+    return exchangeCurrency();
+  }
+};
+export {exchangeCurrency};
---
+++ global-bank/service/show-exchange-rate.service.js
@@ -0,0 +1,55 @@
+import { MESSAGE } from "../const/message.const.js";
+import { checkExit } from "../utils/check-exit.utils.js";
+import { promiseReadLine } from "../../common/utils/promise-readline.utils.js";
+import { checkIsItCurrency } from "../utils/check-isit-currency.utils.js";
+import { EXCHANGE_ACTION_MAP } from "../const/action-map.const.js";
+import { ENABLED_CURRENCIES } from "../const/currency.const.js";
+import { compareExchangeRate } from "../helper/compare-exchange-rate.helper.js";
+import { exchangeComparing } from "../helper/exchage-comparing.helper.js";
+import { updateAccount } from "../repository/update-account.repository.js";
+import { findCurrencyAmount } from "../repository/find-money-amount.repository.js";
+import { showAccount } from "./inquiry.service.js";
+
+/**
+ * retry 는 환율은 이미 조회했는지를 판단하는 boolean 값
+ */
+
+
+const showExchangeRate = async () => {
+  try {
+    const mainCurrency = await promiseReadLine(
+      MESSAGE.EXCHANGE.INPUT_MAIN_CURRENCY,
+    );
+    checkExit(mainCurrency);
+    const upperCurrency = checkIsItCurrency(mainCurrency);
+    if (!ENABLED_CURRENCIES.map((c) => c.currency).includes(upperCurrency)) {
+      throw new Error(MESSAGE.EXCHANGE.RETRY);
+    }
+    const exchangeRateMap = ENABLED_CURRENCIES.reduce((acc, curr) => {
+      return {
+        ...acc,
+        [curr.currency]: compareExchangeRate(upperCurrency, curr.currency),
+      };
+    }, {});
+
+    const targetCurrency = ENABLED_CURRENCIES.find(
+      (c) => c.currency === upperCurrency,
+    );
+    const targetBaseAmount = targetCurrency.baseAmount;
+    const shownList = ENABLED_CURRENCIES.filter(
+      (c) => c.currency !== upperCurrency,
+    );
+    const shownListBaseAmount = shownList.map((c) => c.baseAmount);
+    console.log(
+      `${shownList.map((c) => `${c.baseAmount}${c.currency}${c.to_KR} : ${exchangeRateMap[c.currency] * [c.baseAmount]}${upperCurrency}${targetCurrency.to_KR}`).join("\n")}`,
+    );
+    return activateExchangeController(true);
+  } catch (e) {
+    console.log(e.message);
+    return showExchangeRate();
+  }
+};
+
+
+
+export {showExchangeRate};
