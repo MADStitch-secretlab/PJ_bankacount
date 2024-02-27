e47d199f5694c23077beda5bb2775d4e0eeb80de
MADStitch-secretlab <limdonghyuk12@gmail.com>
2024-02-27 14:00:31 +0900

    refactor : 환전기능 컨트롤러 추가

--- global-bank/controller/global-bank.controller.js
+++ global-bank/controller/global-bank.controller.js
@@ -2,7 +2,7 @@ import { ACTION_MAP, EXCHANGE_ACTION_MAP } from "../const/action-map.const.js";
 import { promiseReadLine } from "../../common/utils/promise-readline.utils.js";
 import { MESSAGE } from "../const/message.const.js";
 import { checkExit } from "../utils/check-exit.utils.js";
-async function selectProgram() {
+async function activateMainController() {
   const answer = await promiseReadLine(MESSAGE.SELECTPROGRAM);
 
   for (const action of Object.values(ACTION_MAP)) {
@@ -16,23 +16,32 @@ async function selectProgram() {
   }
 
   console.log(MESSAGE.RETRY);
-  return selectProgram();
+  return activateMainController();
 }
 
-export { selectProgram };
 
-/*
-* 1. helper, service, repository 레이어 다시 분리
-*    - exchangeComparing 등
-* 2. exchange 함수 service layer -> controller layer로 수정
-* 3. selectProgram 에서 sub-controller 수행 로직 추가
-* 4. showExchangeRate, exchangeCurrency service 파일 분리
-* 5. updateRepository 입력값 수정
-*    - await updateRepository({ KRW : 30000, USD : 100 })
-* 6. repository 함수명 수정
-*    - get, find, create, delete, update
-*    - updateRepository -> updateAccount
-* 7. random 함수 변경 적용 crypto, bcrypto
-* 8. checkCurrency 입력값 수정
-*    - await checkCurrency(["KRW", "USD"])
-* */
+const activateExchangeController = async (retry) => {
+  try {
+    const answer = await promiseReadLine(
+        retry
+            ? MESSAGE.EXCHANGE.SELECT_EXCHANGE_OR_OTHER
+            : MESSAGE.EXCHANGE.SELECT_SECTION,
+    );
+
+    checkExit(answer);
+
+    for (const action of Object.values(EXCHANGE_ACTION_MAP)) {
+      if (action.keys.includes(answer)) {
+        await action.action();
+        return;
+      }
+    }
+  } catch (e) {
+    console.log(e.message);
+    return activateExchangeController();
+  }
+}
+
+
+
+export { activateMainController, activateExchangeController};
